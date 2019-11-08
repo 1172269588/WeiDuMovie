24c0dfec9443308ed3f9b71fb3267572975c9f78
wangshuai166 <54694473+wangshuai166@users.noreply.github.com>
2019-11-08 15:01:32 +0800

    定位

--- app/src/main/java/com/bawie/weidu_movie/view/fragment/FragmentMovie.java
+++ app/src/main/java/com/bawie/weidu_movie/view/fragment/FragmentMovie.java
@@ -1,7 +1,13 @@
 package com.bawie.weidu_movie.view.fragment;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,13 +16,21 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.location.AMapLocationListener;
 import com.bawie.weidu_movie.R;
 import com.bawie.weidu_movie.model.bean.HotMovieBean;
 import com.bawie.weidu_movie.model.bean.IsHotMovieBean;
@@ -34,6 +48,8 @@ import com.bumptech.glide.Glide;
 import com.stx.xhb.xbanner.XBanner;
 import com.stx.xhb.xbanner.transformers.Transformer;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import butterknife.BindView;
@@ -73,6 +89,8 @@ public class FragmentMovie extends Fragment implements IContractView.iMovieView,
     RecyclerView recyclerUpComing;
     @BindView(R.id.recycler_hot)
     RecyclerView recyclerHot;
+    @BindView(R.id.textdw)
+    TextView textdw;
     private int page = 1;
     private int count = 14;
     private Unbinder unbinder;
@@ -83,6 +101,9 @@ public class FragmentMovie extends Fragment implements IContractView.iMovieView,
     private RecyclerUpComingAdapter recyclerUpComingAdapter;
     private List<HotMovieBean.ResultBean> hotResult;
     private RecyclerViewHotAdapter recyclerViewHotAdapter;
+    private AMapLocationClient mlocationClient;
+    private AMapLocationClientOption mLocationOption;
+    private int GPS_REQUEST_CODE = 10;
 
     @Nullable
     @Override
@@ -99,7 +120,117 @@ public class FragmentMovie extends Fragment implements IContractView.iMovieView,
         homePresenter.upComingMovie(page, count);
         //热门电影
         homePresenter.hotMovie(page, count);
+        imgLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED) {//未开启定位权限
+                    //开启定位权限,200是标识码
+                    ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 200);
+                } else {
+                    MyLocation(getActivity());//开始定位
+                }
+            }
+
+        });
         return inflate;
+
+    }
+
+    public void MyLocation(final Context context) {
+        mlocationClient = new AMapLocationClient(getActivity());
+        mLocationOption = new AMapLocationClientOption();
+        mlocationClient.setLocationListener(new AMapLocationListener() {
+            @Override
+            public void onLocationChanged(AMapLocation amapLocation) {
+                try {
+                    if (amapLocation != null) {
+                        if (amapLocation.getErrorCode() == 0) {
+                            //定位成功回调信息，设置相关消息
+
+                            //获取当前定位结果来源，如网络定位结果，详见定位类型表
+                            Log.i("定位类型", amapLocation.getLocationType() + "");
+                            Log.i("获取纬度", amapLocation.getLatitude() + "");
+                            Log.i("获取经度", amapLocation.getLongitude() + "");
+                            Log.i("获取精度信息", amapLocation.getAccuracy() + "");
+                            //如果option中设置isNeedAddress为false，则没有此结果，网络定位结果中会有地址信息，GPS定位不返回地址信息。
+                            Log.i("地址", amapLocation.getAddress());
+                            Log.i("国家信息", amapLocation.getCountry());
+                            Log.i("省信息", amapLocation.getProvince());
+                            Log.i("城市信息", amapLocation.getCity());
+                            Log.i("城区信息", amapLocation.getDistrict());
+                            Log.i("街道信息", amapLocation.getStreet());
+                            Log.i("街道门牌号信息", amapLocation.getStreetNum());
+                            Log.i("城市编码", amapLocation.getCityCode());
+                            Log.i("地区编码", amapLocation.getAdCode());
+                            Log.i("获取当前定位点的AOI信息", amapLocation.getAoiName());
+                            Log.i("获取当前室内定位的建筑物Id", amapLocation.getBuildingId());
+                            Log.i("获取当前室内定位的楼层", amapLocation.getFloor());
+                            Log.i("获取GPS的当前状态", amapLocation.getGpsAccuracyStatus() + "");
+                            //获取定位时间
+                            SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                            Date date = new Date(amapLocation.getTime());
+                            Log.i("获取定位时间", df.format(date));
+                            textdw.setText(amapLocation.getDistrict());
+                            textdw.setTextColor(Color.parseColor("#FFFFFF"));
+                            // 停止定位
+                            mlocationClient.stopLocation();
+                        } else {
+                            //定位失败时，可通过ErrCode（错误码）信息来确定失败的原因，errInfo是错误信息，详见错误码表。
+                            Log.e("AmapError", "location Error, ErrCode:"
+                                    + amapLocation.getErrorCode() + ", errInfo:"
+                                    + amapLocation.getErrorInfo());
+                            Toast.makeText(context, "没有权限，请打开权限...", Toast.LENGTH_SHORT).show();
+                            new AlertDialog.Builder(context)
+                                    .setTitle("定位服务未开启")
+                                    .setMessage("请在系统设置中开启定位服务\n" +
+                                            "以便为您提供更好的服务")
+                                    .setPositiveButton("设置", new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                                            startActivityForResult(intent, GPS_REQUEST_CODE);
+                                        }
+                                    })
+                                    .show();
+                        }
+                    }
+                } catch (Exception e) {
+                }
+
+            }
+        });
+        //设置定位模式为高精度模式，Battery_Saving为低功耗模式，Device_Sensors是仅设备模式
+        mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
+        mLocationOption.setInterval(5000);
+        //设置定位参数
+        mlocationClient.setLocationOption(mLocationOption);
+        //启动定位
+        mlocationClient.startLocation();
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {//未开启定位权限
+            //开启定位权限,200是标识码
+            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 200);
+
+        } else {
+            MyLocation(getActivity());//开始定位
+            //Toast.makeText(getActivity(),"已开启定位权限",Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        // 停止定位
+        if (null != mlocationClient) {
+            mlocationClient.stopLocation();
+        }
 
     }
     //banner
